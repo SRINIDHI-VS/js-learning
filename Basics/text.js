54fc927f4b7de96557963f7fc49000c9af6f4503
V S SRINIDHI <76029867+SRINIDHI-VS@users.noreply.github.com>
2024-07-07 04:24:55 +0000

    Done

--- Basics/text.js
+++ Basics/text.js
@@ -1,1 +1,24 @@
-console.log('HEllo Shree');
+// console.log('HEllo Shree');
+const accountdID = 14366562;
+let accountEmail = 'shree@gmail.com';
+accountName = 'shree';
+
+practice = true;
+practice2 = 4687587135876473154713548758743587135487135481354374n;
+practice3 = undefined
+practice4 = null
+
+
+console.table([accountdID,accountEmail,accountName]);
+console.log(accountdID,accountEmail,accountName);
+
+// console.log(typeof null);
+// console.log(typeof BigInt);
+// console.log(typeof NaN);
+// console.log(typeof Boolean)
+
+console.log(typeof accountdID)
+console.log(typeof practice)
+console.log(typeof practice2)
+console.log(typeof practice3)
+console.log(typeof practice4)
