e1429b44cd31ab88cca8c810c07b95ece7260fb0
V S SRINIDHI <76029867+SRINIDHI-VS@users.noreply.github.com>
2024-07-07 06:17:28 +0000

    Done

---
+++ Basics/strings.js
@@ -0,0 +1,31 @@
+// `use strict`
+// // // // 
+// // // const str = String('shree');
+
+// // // console.log(str);
+// // // console.log(typeof str);
+
+// // const str = `dbfksfk skd         \n  \n\n\n\n\n
+// //  kf baefeabfuef           
+// //  ehfubhweiufhwef `;
+
+// // console.log(str.trim())
+
+// // const str = `shreevssrinidhi`;
+// // console.log(str.trim().slice(-1,3));
+
+// // // console.log('shree')
+
+// const str = `hndfi w   ihf ief e     h wefohweo`
+// // console.log(str.split(' '))
+// // console.log(str.slice(-0,5))
+// console.log(str.at(4))
+// console.log(str.at(-5))
+// console.log(str.charAt(6))
+
+const str = `I am shree shree`;
+console.log(str.endsWith('shree'));
+console.log(str.includes('shree'));
+console.log(str.search('shree'));
+console.log(str.indexOf('shree'));
+console.log(str.replace('shree', 'shravs'));
